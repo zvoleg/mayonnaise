273e35cbebc453b1d9bb33806c42f007fadf4c14
Oleg <oleg.rus178@gmail.com>
2019-11-19 16:47:56 +0300

    add cartridge space to bus

--- src/bus.rs
+++ src/bus.rs
@@ -17,20 +17,25 @@ impl Bus {
     }
 
     pub fn read_cpu_ram(&self, address: u16) -> u8 {
-        if address >= 0 && address <= 0x1FFF {
+        if address <= 0x1FFF {
             return self.cpuRam[(address & 0x07FF) as usize];
         } else if address >= 0x2000 && address <= 0x3FFF {
             return self.ppuRegister[(address & 0x0007) as usize];
+        } else if address >= 0x4020 && address <= 0xFFFF {
+            // cartrige space
+        }
         } else {
             0
         }
     }
 
     pub fn write_cpu_ram(&mut self, address: u16, data: u8) {
-        if address >= 0 && address <= 0x1FFF {
+        if address <= 0x1FFF {
             self.cpuRam[(address & 0x07FF) as usize] = data;
         } else if address >= 0x2000 && address <= 0x3FFF {
             self.ppuRegister[(address & 0x0007) as usize] = data;
+        } else if address >= 0x4020 && address <= 0xFFFF {
+            // cartrige space maybe need access to mapper
         }
     }
 }
